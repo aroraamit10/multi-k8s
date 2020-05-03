73a8f771b25ef2c8fe28bd2d0571745074a7a90d
B0212977 <b0212977@B0212977s-MacBook-Pro.local>
2020-05-03 14:19:24 +0530

    change docker login

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t aroraamit10/multi-client:latest -t aroraamit10/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t aroraamit10/multi-server:latest -t aroraamit10/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t aroraamit10/multi-worker:latest -t aroraamit10/multi-worder:$SHA -f ./worker/Dockerfile ./worker
+docker build -t amitairtel09/multi-client:latest -t amitairtel09/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t amitairtel09/multi-server:latest -t amitairtel09/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t amitairtel09/multi-worker:latest -t amitairtel09/multi-worder:$SHA -f ./worker/Dockerfile ./worker
 
-docker push aroraamit10/multi-client:latest
-docker push aroraamit10/multi-server:latest
-docker push aroraamit10/multi-worker:latest
+docker push amitairtel09/multi-client:latest
+docker push amitairtel09/multi-server:latest
+docker push amitairtel09/multi-worker:latest
 
-docker push aroraamit10/multi-client:$SHA
-docker push aroraamit10/multi-server:$SHA
-docker push aroraamit10/multi-worker:$SHA
+docker push amitairtel09/multi-client:$SHA
+docker push amitairtel09/multi-server:$SHA
+docker push amitairtel09/multi-worker:$SHA
 
 kubectl delete -f k8s
-kubectl set image deployments/server-deployment server=aroraamit10/multi-server:$SHA
-kubectl set image deployments/client-deployment server=aroraamit10/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=aroraamit10/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=amitairtel09/multi-server:$SHA
+kubectl set image deployments/client-deployment server=amitairtel09/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=amitairtel09/multi-worker:$SHA
